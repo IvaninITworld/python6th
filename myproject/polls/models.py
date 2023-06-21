e6455fca9f1130e587056cb238dd718cebd9cd1e
ivan <xormrdlsrks2@gmail.com>
2023-06-21 19:54:33 +0900

    Django 웹 프로그래밍 실습 - polls 만들고 View 에 연결해서 웹페이지 동적으로 동작하기 까지

--- myproject/polls/models.py
+++ myproject/polls/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Question(models.Model):
-    questions_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.questions_text
+        return self.question_text
 
 
 class Choice(models.Model):
