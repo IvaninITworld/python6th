dd3f90a3aa81d0efafb2a0dfab8b6470de3170af
ivan <xormrdlsrks2@gmail.com>
2023-06-23 11:50:20 +0900

    book_detail 페이지 뷰

--- myproject/books/urls.py
+++ myproject/books/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path("author/", views.AuthorList.as_view(), name="author_list"),
     path("publisher/", views.PublisherList.as_view(), name="publisher_list"),
 
+    path("books/<int:pk>", views.BookDetail.as_view(), name="book_detail"),
+    path("author/<int:pk>", views.AuthorDetail.as_view(), name="author_detail"),
+    path("publisher/<int:pk>", views.PublisherDetail.as_view(), name="publisher_detail"),
+
 ]
