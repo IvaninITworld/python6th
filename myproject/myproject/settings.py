42ee683430c8ecbb59302b535a09ed919b69ef11
ivan <xormrdlsrks2@gmail.com>
2023-06-23 09:14:34 +0900

    Books 앱 추가

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     "polls.apps.PollsConfig",
+    "books.apps.BooksConfig"
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
